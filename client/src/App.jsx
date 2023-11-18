e16c208985414e3464b0d1ecd9cb62b144a0c32c
Anton1636 <you@example.comhobermac228@gmail.com>
2023-11-18 23:24:17 +0200

    Added main page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,10 +3,9 @@ import { Footer, Navbar, Services, Transactions, Welcome } from './components'
 const App = () => {
 	return (
 		<div className='min-h-screen'>
-			<div className='gradient-bg-welcome'>
+			<div className='gradient-bg-welcome '>
 				<Navbar />
 				<Welcome />
-				<h1 className='text-3xl font-bold underline'>Hello world!</h1>
 			</div>
 			<Services />
 			<Transactions />
